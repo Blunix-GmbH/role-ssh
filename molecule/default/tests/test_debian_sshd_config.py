48da1638faa121b73d40b6d7d478476420d6a695
Brian Wiborg <baccenfutter@c-base.org>
2018-01-08 16:06:19 +0100

    fix #8

--- molecule/default/tests/test_debian_sshd_config.py
+++ molecule/default/tests/test_debian_sshd_config.py
@@ -3,7 +3,7 @@ import os
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('debian')
 
 
 def test_ssh_config(File):
@@ -13,19 +13,17 @@ def test_ssh_config(File):
     assert f.contains('ListenAddress 0.0.0.0')
     assert f.contains('Protocol 2')
     assert f.contains('UsePrivilegeSeparation yes')
-    assert f.contains('ServerKeyBits 1024')
     assert f.contains('LogLevel INFO')
     assert f.contains('LoginGraceTime 120')
     assert f.contains('PermitRootLogin without-password')
     assert f.contains('StrictModes yes')
     assert f.contains('PubkeyAuthentication yes')
     assert f.contains('IgnoreRhosts yes')
-    assert f.contains('RhostsRSAAuthentication no')
     assert f.contains('HostbasedAuthentication no')
     assert f.contains('PermitEmptyPasswords no')
     assert f.contains('ChallengeResponseAuthentication no')
     assert f.contains('PasswordAuthentication no')
-    assert f.contains('X11Forwarding yes')
+    assert f.contains('X11Forwarding no')
     assert f.contains('X11DisplayOffset 10')
     assert f.contains('PrintMotd no')
     assert f.contains('PrintLastLog yes')
