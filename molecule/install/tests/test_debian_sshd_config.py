b82d15749c726c30bf0d377386b71aba8d7ac6f0
Peter Thurner <p.thurner@blunix.org>
2018-05-27 10:54:30 +0200

    added old test again

---
+++ molecule/install/tests/test_debian_sshd_config.py
@@ -0,0 +1,32 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('debian')
+
+
+def test_ssh_config(File):
+    f = File('/etc/ssh/sshd_config')
+
+    assert f.exists
+    assert f.contains('ListenAddress 0.0.0.0')
+    assert f.contains('Protocol 2')
+    assert f.contains('UsePrivilegeSeparation yes')
+    assert f.contains('LogLevel INFO')
+    assert f.contains('LoginGraceTime 120')
+    assert f.contains('PermitRootLogin without-password')
+    assert f.contains('StrictModes yes')
+    assert f.contains('PubkeyAuthentication yes')
+    assert f.contains('IgnoreRhosts yes')
+    assert f.contains('HostbasedAuthentication no')
+    assert f.contains('PermitEmptyPasswords no')
+    assert f.contains('ChallengeResponseAuthentication no')
+    assert f.contains('PasswordAuthentication no')
+    assert f.contains('X11Forwarding no')
+    assert f.contains('X11DisplayOffset 10')
+    assert f.contains('PrintMotd no')
+    assert f.contains('PrintLastLog yes')
+    assert f.contains('TCPKeepAlive yes')
+    assert f.contains('UsePAM yes')
+    assert f.contains('UseDNS no')
--- molecule/install/tests/test_presense.py
+++ molecule/install/tests/test_presense.py
@@ -5,5 +5,6 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-def test_executable_exists(host):
-    assert host.file('/usr/bin/ssh').exists
+
+def test_package(host):
+    assert host.package('openssh-server').is_installed
